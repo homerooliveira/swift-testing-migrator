f0a84e462b7c9bd14bc679f52f84df2e225ea8c7
Homero Oliveira <homero_oliveira@icloud.com>
2025-05-21 22:07:07 -0300

    Refactor SwiftTestingMigrator to read source file path and handle dry run; streamline output handling

--- Sources/swift-testing-migrator/SwiftTestingMigrator.swift
+++ Sources/swift-testing-migrator/SwiftTestingMigrator.swift
@@ -6,59 +6,26 @@ import TestingMigrator
 
 @main
 struct SwiftTestingMigrator: AsyncParsableCommand {
+
+    @Argument(help: "The path to the source file to be migrated.")
+    var sourceFilePath: String
+    @Argument(help: "The path to the destination file.")
+    var destinationFilePath: String?
+    @Flag(help: "Perform a dry run without making changes.")
+    var dryRun: Bool = false
+
     mutating func run() async throws {
-        let source = """
-        import Foundation
-        import XCTest
-        
-        final class _ArrayExtensionsTests: XCTestCase {
-            func testSubscriptGet() {
-                let numbers = [0, 1]
-                
-                XCTAssertEqual(numbers[0], 0)
-                XCTAssertNotEqual(numbers[1], 2)
-                XCTAssertNotEqual(numbers[1], 2, "Must be different")
-                XCTAssertNotEqual(numbers[1], 2, "Must be different", file: #file, line: #line)
-                XCTAssertNotEqual(numbers[1], 2, line: #line)
-                
-                let n = NSString()
-                let n1 = n
-                let n2 = NSString("1")
-                XCTAssertIdentical(n, n1)
-                XCTAssertNotIdentical(n, n2)
-                
-                XCTAssertGreaterThan(numbers[1], 0)
-                XCTAssertGreaterThanOrEqual(numbers[1], 0)
-                XCTAssertLessThanOrEqual(numbers[0], 1)
-                XCTAssertLessThan(numbers[0], 1)
-                
-                XCTAssert(numbers[1] == 1)
-                XCTAssertTrue(numbers[0] == 0)
-                XCTAssertFalse(numbers[0] == 2)
-                XCTAssertFalse(false)
-        
-                XCTAssertNil(
-                    nil,
-                    "Must be nil"
-                )
-                XCTAssertNotNil(1)
-                XCTFail("Test")
-        
-                let x = try XCTUnwrap(.some(1))
-            }
-            
-            func testSubscriptSet() async throws {
-                var numbers = [0, 1]
-                
-                numbers[0] = 12
-                numbers[1] = 13
-                
-                XCTAssertEqual(numbers, [12, 13])
-            }
+        let source = try String(contentsOfFile: sourceFilePath)
+        let rewriter = Rewriter(.init(useClass: false))
+        let modifiedSource = rewriter.rewrite(source: source).description
+
+        if dryRun || destinationFilePath == nil {
+            print("Dry run: No changes made.")
+            print("Modified source:\n\(modifiedSource)")
+        } else if let destinationFilePath  {
+            let destinationFileURL = URL(fileURLWithPath: destinationFilePath)
+            try modifiedSource.write(to: destinationFileURL, atomically: true, encoding: .utf8)
+            print("Changes made to \(destinationFilePath).")
         }
-        """
-        let modifiedSourceFile = Rewriter().rewrite(source: source)
-        
-        print(modifiedSourceFile)
     }
 }
